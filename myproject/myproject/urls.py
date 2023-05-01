a11703792ce2e1cb7212909e70af4dc8fecb23ce
Cristhian1620 <75464976+Cristhian47@users.noreply.github.com>
2023-05-01 16:11:36 -0500

    html testing

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 def testing_method():
     views.saludo
 
