1ebb54dec962f4bc9fd7f155312232fe8cd01940
jsanti123 <89226676+jsanti123@users.noreply.github.com>
2023-05-08 19:25:41 -0500

    Actualizacion_servidor_version_0.0
    Se genero la estructura principal del servidor, y se envia a los clientes , los catalogos de colores y ademas ya por parte del cliente se puede elegir el color

--- Servidor_compatible.py
+++ Servidor_compatible.py
@@ -1,32 +1,85 @@
 import socket
+import threading
 import json
 
+# Datos del servidor
 HOST = 'localhost'  # El host del servidor
-PORT = 8888        # El puerto del servidor
+PORT = 8001        # El puerto del servidor
 
-# Crea un socket y lo vincula al host y al puerto especificados
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Servidor esperando conexiones en {HOST}:{PORT}...")
+# Conectarse al servidor
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((HOST, PORT))
+servidor.listen(5)
+print(f"Servidor esperando conexiones en {HOST}:{PORT}...")
 
-    # Espera a que un cliente se conecte
-    conn, addr = s.accept()
-    with conn:
+#Diccionario para manejar los colores disponibles
+colores_disponibles = {"Yellow": True , "Blue": True, "Green": True, "Red": True}
+
+#lista de hilos para los clientes
+clientes = []
+hilos_clientes = []
+
+#Clase para manejar a los clientes
+class ManejarCliente(threading.Thread):
+    def __init__(self, conn):
+        super(ManejarCliente, self).__init__()
+        self.conn = conn
+        self.nombre = ""
+        self.color = ""
+
+    #Que es lo  que viene en el mensaje
+    def procesar_informacion(self, msg):
+        informacion = json.loads(msg)
+
+        #el cliente solicita los colores disponibles
+        if informacion["tipo"] == "solicitud_color":
+            respuesta = colores_disponibles
+            self.enviar_respuesta(respuesta)
+        #el cliente selecciona un color
+        elif informacion["tipo"] == "seleccion_color":
+            if colores_disponibles[informacion["color"]]:
+                colores_disponibles[informacion["color"]] = False
+                self.name = informacion["nombre"]
+                self.color = informacion["color"]
+                respuesta = {"color": informacion["color"], "disponible": True}
+                self.enviar_respuesta(respuesta)
+            else:
+                respuesta = {"color": informacion["color"], "disponible": False}
+                self.enviar_respuesta(respuesta)
+
+    #Funcion para enviar una respuesta al cliente
+    def enviar_respuesta(self, informacion):
+        respuesta = json.dumps(informacion)
+        self.conn.sendall(respuesta.encode('utf-8'))
+
+    #Funcion que se ejecuta cuando se inicia el hilo
+    def run(self):
+        while True:
+            # Recibe los datos del cliente
+            msg = self.conn.recv(1024).decode()
+            if msg:
+                self.procesar_informacion(msg)
+        
+    #Funcion para enviar un mensaje a todos los clientes
+    def broadcast(self, msg):
+            for client in hilos_clientes:
+                client.send(msg.encode())
+    
+#Ciclo para aceptar clientes    
+while True:
+    if len(hilos_clientes) < 4:
+        # Espera a que un cliente se conecte
+        conn, addr = servidor.accept()
         print('Conexión establecida por', addr)
-
-        # Recibe los datos del cliente
-        data = conn.recv(1024)
-        # Decodifica el mensaje JSON
-        msg = json.loads(data.decode('utf-8'))
-        print(f"Mensaje recibido: {msg}")
-
-        # Procesa los datos
-        respuesta = {"resultado": msg["valor1"] + msg["valor2"]}
-
-        # Codifica la respuesta en formato JSON
-        respuesta_json = json.dumps(respuesta)
-        print(f"Respuesta enviada: {respuesta_json}")
-
-        # Envía la respuesta al cliente
-        conn.sendall(respuesta_json.encode('utf-8'))
+        mensaje = "Conexión establecida con el servidor."
+        conn.sendall(mensaje.encode('utf-8'))
+        # Crea un hilo para manejar al cliente
+        thread = ManejarCliente(conn)
+        clientes.append(conn)
+        hilos_clientes.append(thread)
+        thread.start()
+    else:
+        conn, addr = servidor.accept()
+        mensaje = "No se pueden aceptar más clientes"
+        conn.sendall(mensaje.encode('utf-8'))
+        conn.close()
